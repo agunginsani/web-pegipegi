fa381910846ed0bf31cfcd5c4358c314aaa197c0
Agung Insani <agunginsanialam@gmail.com>
2022-08-30 09:53:28 +0700

    feat: bump nuxt version and add pegipegi-web-ui

---
+++ apps/bus-mobile/app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Button, Input } from '@pegipegi/pegipegi-web-ui';
+</script>
+
+<template>
+  <div>
+    <p>Hello, World!</p>
+    <Button>Click me</Button>
+    <Input />
+  </div>
+</template>
