1f0a95630bcd582054f7031a0f37019161f6ffd3
Agung Insani <agunginsanialam@gmail.com>
2022-10-03 17:09:12 +0700

    chore: update dependencies

--- apps/flight-mobile/app.vue
+++ apps/flight-mobile/app.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
-import { Button } from '@pegipegi/pegipegi-web-ui';
+import { Button, Rating, Input } from '@pegipegi/pegipegi-web-ui';
+const rating = ref(0);
+const input = ref('');
 </script>
 
 <template>
   <div class="">
     <Button>Hello</Button>
+    <Rating v-model="rating" />
+    <Input v-model="input" />
   </div>
 </template>
