ceddd5da209301de3e03716f7ba78345546048e3
willywnt <willywinata1688@gmail.com>
2022-12-26 13:38:20 +0700

    feat: install nuxt image

--- apps/flight-mobile/nuxt.config.ts
+++ apps/flight-mobile/nuxt.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    "@nuxt/image-edge",
     "@nuxtjs/tailwindcss",
     ["@pinia/nuxt", { autoImports: ["defineStore"] }],
   ],
