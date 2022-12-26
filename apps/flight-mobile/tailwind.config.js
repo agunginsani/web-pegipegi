58a5d362a1cba4c1726c35cd39dd078c47cd8678
willywnt <willywinata1688@gmail.com>
2022-12-26 10:53:43 +0700

    feat: init tailwind and assets

--- apps/flight-mobile/tailwind.config.js
+++ apps/flight-mobile/tailwind.config.js
@@ -2,6 +2,10 @@
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
-  content: ["./**/@pegipegi/web-pegipegi-ui/**/*.js", "./pages/**/*.vue"],
+  content: [
+    "./**/@pegipegi/web-pegipegi-ui/**/*.js",
+    "./pages/**/*.vue",
+    "./modules/**/*.vue",
+  ],
   presets: [require("@pegipegi/web-pegipegi-ui/tailwind-config")],
 };
