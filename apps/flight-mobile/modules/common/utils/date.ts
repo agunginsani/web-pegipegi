3e0b1a622fcfb6ca4ccc40ed0c269558f47d1d51
Andi Ersaldy Raisha Pakki <andi.ersaldy@pegipegi.com>
2023-01-02 16:25:19 +0800

    chore: remove unused todo

--- apps/flight-mobile/modules/common/utils/date.ts
+++ apps/flight-mobile/modules/common/utils/date.ts
@@ -1,4 +1,3 @@
-// TODO: only import used functions or find a way to treeshake this library
 import * as dateFns from 'date-fns';
 // TODO: fix type definition
 import { id } from 'date-fns/locale/index.js';
