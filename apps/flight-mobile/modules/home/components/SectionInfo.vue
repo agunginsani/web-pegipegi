5f675c366c37dae67555e785647485d1c77feb94
inganta23 <johanes230500@gmail.com>
2023-01-02 22:38:44 +0700

    feat: remove unused tag

--- apps/flight-mobile/modules/home/components/SectionInfo.vue
+++ apps/flight-mobile/modules/home/components/SectionInfo.vue
@@ -5,38 +5,36 @@
 </script>
 
 <template>
-  <section>
-    <div class="pl-4">
-      <h1 class="font-bold">Informasi Penting</h1>
-      <ul class="mt-2">
-        <li
-          class="border-neutral-tuna-50 border-b py-3.5"
-          v-for="(info, index) in infos"
-          :key="index"
+  <section class="pl-4">
+    <h1 class="font-bold">Informasi Penting</h1>
+    <ul class="mt-2">
+      <li
+        class="border-neutral-tuna-50 border-b py-3.5"
+        v-for="(info, index) in infos"
+        :key="index"
+      >
+        <a
+          class="relative flex items-center pr-4"
+          :href="`${info.landingPageUrl}`"
+          target="_blank"
         >
-          <a
-            class="relative flex items-center pr-4"
-            :href="`${info.landingPageUrl}`"
-            target="_blank"
-          >
-            <NuxtImg
-              class="mr-2"
-              :src="`${info.imageUrlIcon}`"
-              :alt="`${info.description}`"
-              width="28px"
-              height="28px"
-            />
-            <p class="text-neutral-tuna-300 text-sm">
-              {{ info.description }}
-            </p>
-            <NuxtImg
-              class="ml-auto"
-              src="/icon-chevron-right-orange.svg"
-              alt="icon"
-            />
-          </a>
-        </li>
-      </ul>
-    </div>
+          <NuxtImg
+            class="mr-2"
+            :src="`${info.imageUrlIcon}`"
+            :alt="`${info.description}`"
+            width="28px"
+            height="28px"
+          />
+          <p class="text-neutral-tuna-300 text-sm">
+            {{ info.description }}
+          </p>
+          <NuxtImg
+            class="ml-auto"
+            src="/icon-chevron-right-orange.svg"
+            alt="icon"
+          />
+        </a>
+      </li>
+    </ul>
   </section>
 </template>
