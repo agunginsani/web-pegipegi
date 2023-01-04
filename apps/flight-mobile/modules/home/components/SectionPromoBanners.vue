c4ee3963075c9324c5ce08fab11adf15c2d00507
Fauzan Khalif <fauzan.khalif@pegipegi.com>
2023-01-04 17:47:59 +0700

    chore: change promo banners

--- apps/flight-mobile/modules/home/components/SectionPromoBanners.vue
+++ apps/flight-mobile/modules/home/components/SectionPromoBanners.vue
@@ -1,66 +1,13 @@
 <script lang="ts" setup>
-  import PromoBanners from './PromoBanners.vue';
-
-  const promoBanners = ref([
-    {
-      id: 1,
-      image:
-        'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80',
-    },
-    {
-      id: 2,
-      image:
-        'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80',
-    },
-    {
-      id: 3,
-      image:
-        'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80',
-    },
-    {
-      id: 4,
-      image:
-        'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80',
-    },
-  ]);
+  import PromoBanners from 'home-module/components/PromoBanners.vue';
 </script>
 
 <template>
   <section aria-labelledby="promo-banners-user" class="pb-3">
     <div class="mb-4 flex justify-between px-4">
       <h1 id="promo-banners-user" class="text-base font-bold">Promo</h1>
-      <button class="font-bold text-[#FE5000]">Lihat Semua</button>
+      <button class="text-orange-inter-600 font-bold">Lihat Semua</button>
     </div>
-    <ul
-      class="no-scroll-bar flex w-full snap-x snap-mandatory space-x-2 overflow-x-auto px-4"
-    >
-      <PromoBanners
-        v-for="banner in promoBanners"
-        :key="banner.id"
-        :banner="banner"
-      />
-    </ul>
-    <ul class="mt-2 flex justify-center gap-x-1 align-middle">
-      <li
-        v-for="banner in promoBanners"
-        :key="banner.id"
-        class="bg-neutral-tuna-300 h-1.5 w-1.5 rounded-full"
-      ></li>
-    </ul>
+    <PromoBanners />
   </section>
 </template>
-
-<style scoped>
-  /* Hide scrollbar for Chrome, Safari and Opera */
-  .no-scroll-bar::-webkit-scrollbar {
-    display: none;
-  }
-
-  /* Hide scrollbar for IE, Edge and Firefox */
-  .no-scroll-bar {
-    -ms-overflow-style: none;
-    /* IE and Edge */
-    scrollbar-width: none;
-    /* Firefox */
-  }
-</style>
