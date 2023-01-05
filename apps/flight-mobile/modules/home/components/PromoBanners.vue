3595bfa74e0719e047898f9f812b2760ab079672
Fauzan Khalif <fauzan.khalif@pegipegi.com>
2023-01-05 14:47:40 +0700

    chore: remove vue use component

--- apps/flight-mobile/modules/home/components/PromoBanners.vue
+++ apps/flight-mobile/modules/home/components/PromoBanners.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { vIntersectionObserver } from '@vueuse/components';
+  import { useIntersectionObserver } from '@vueuse/core';
 
   const promoBanners = ref([
     {
@@ -24,45 +24,77 @@
         'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80',
     },
   ]);
-
   const root = ref(null);
-
+  const target = ref(null);
   const isVisible = ref(false);
+  console.log(root);
+  console.log(target);
 
-  function onIntersectionObserver([{ isIntersecting }]) {
-    isVisible.value = isIntersecting;
-    console.log(isVisible);
-  }
+  useIntersectionObserver(
+    target,
+    ([{ isIntersecting }]) => {
+      isVisible.value = isIntersecting;
+      console.log('Hello');
+    },
+    { root }
+  );
 </script>
 
 <template>
   <ul
     class="no-scroll-bar flex w-full snap-x snap-mandatory space-x-2 overflow-x-auto px-4"
+    ref="root"
   >
-    <li
-      v-for="banner in promoBanners"
-      :key="banner.id"
-      class="snap-center snap-always"
-    >
+    <li class="snap-center snap-always">
       <div
         class="relative min-w-[312px] flex-shrink-0 overflow-hidden rounded-xl"
-        ref="root"
       >
         <NuxtImg
-          :src="banner.image"
+          ref="target"
+          src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80"
           alt="promo-banner"
           width="312"
           height="156"
-          v-intersection-observer="[onIntersectionObserver, { root }]"
+        />
+      </div>
+    </li>
+    <li class="snap-center snap-always">
+      <div
+        class="relative min-w-[312px] flex-shrink-0 overflow-hidden rounded-xl"
+      >
+        <NuxtImg
+          ref="target"
+          src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80"
+          alt="promo-banner"
+          width="312"
+          height="156"
+        />
+      </div>
+    </li>
+    <li class="snap-center snap-always">
+      <div
+        class="relative min-w-[312px] flex-shrink-0 overflow-hidden rounded-xl"
+      >
+        <NuxtImg
+          ref="target"
+          src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=312&h=156&q=80"
+          alt="promo-banner"
+          width="312"
+          height="156"
         />
       </div>
     </li>
   </ul>
+
   <ul class="mt-2 flex justify-center gap-x-1 align-middle">
     <li
       v-for="banner in promoBanners"
       :key="banner.id"
-      class="bg-neutral-tuna-300 h-1.5 w-1.5 rounded-full"
+      :class="[
+        isVisible
+          ? 'bg-orange-inter-600 h-1.5 w-3 rounded-full'
+          : 'bg-neutral-tuna-300 h-1.5 w-1.5 rounded-full',
+      ]"
     ></li>
   </ul>
 </template>
