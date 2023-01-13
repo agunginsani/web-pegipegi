821d14ced23793ae36e5b33ae13e5e13e58880fc
willywnt <willywinata1688@gmail.com>
2023-01-13 17:09:03 +0700

    chore: remove component at index.vue

--- apps/flight-mobile/pages/index.vue
+++ apps/flight-mobile/pages/index.vue
@@ -4,7 +4,6 @@
   import SectionPromo from 'home-module/components/SectionPromo.vue';
   import SectionInfo from 'home-module/components/SectionInfo.vue';
   import SectionCheapFlight from 'home-module/components/SectionCheapFlight.vue';
-  import Swiper from 'home-module/components/Swiper.vue';
   import Navbar from 'home-module/components/Navbar.vue';
   import Footer from 'home-module/components/Footer.vue';
 </script>
@@ -17,18 +16,6 @@
     <SectionPromo />
     <SectionInfo />
     <SectionCheapFlight />
-    <Swiper :indicated="true">
-      <li v-for="(_, index) in 5" :key="index" :data-index="index">
-        <div class="flex flex-col items-center justify-center">
-          <h1>test 1</h1>
-          <h1>test 2</h1>
-          <h1>test 3</h1>
-          <h1>test 4</h1>
-          <h1>test 5</h1>
-          <h1>test 6</h1>
-        </div>
-      </li>
-    </Swiper>
     <Footer />
   </div>
 </template>
