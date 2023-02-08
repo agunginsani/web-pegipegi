1869603b5c992ead2f86c86d1ced34314cec2159
Agung Insani <agunginsanialam@gmail.com>
2023-02-08 14:47:41 +0700

    perf: move head and seo config to app
    so that nuxt.config.ts more focus on nuxt configuration

--- apps/flight-mobile/app.vue
+++ apps/flight-mobile/app.vue
@@ -4,6 +4,56 @@
   import useProfile from 'common-module/composables/use-profile';
   import useAuth from 'common-module/composables/use-auth';
 
+  const title = `Tiket Pesawat Promo - Cek & Pesan Tiket Online Harga OK`;
+  const seoDescription = `Cek jadwal dan promo tiket pesawat murah untuk berbagai rute/maskapai di sini. Pesan tiket pesawat online mudah & OK harganya pakai Pegipegi.`;
+  const seoImage = `https://www.pegipegi.com/flat/img/ogp-pegipegi.jpg`;
+  const seoImageWidth = '398';
+  const seoImageHeight = '220';
+
+  useHead({
+    title,
+    htmlAttrs: {
+      lang: 'id',
+    },
+    link: [
+      {
+        rel: 'shortcut icon',
+        href: 'https://www.pegipegi.com/mobile/homepage/all/img/apple-touch-icon.png',
+        type: 'image/x-icon',
+      },
+      {
+        rel: 'canonical',
+        href: 'https://www.pegipegi.com/flight',
+      },
+    ],
+  });
+
+  useServerSeoMeta({
+    contentType: 'text/html; charset=utf-8',
+    robots: 'noindex, follow',
+    themeColor: '#ffdc8e',
+    description: seoDescription,
+    author: 'PT. GO Online Destination',
+    ogTitle: title,
+    ogType: 'website',
+    ogUrl: 'https://www.pegipegi.com/flight',
+    ogImage: seoImage,
+    ogDescription: seoDescription,
+    ogSiteName: 'Pegipegi',
+    ogImageWidth: seoImageWidth,
+    ogImageHeight: seoImageHeight,
+    fbAppId: '966242223397117',
+    twitterCard: 'summary_large_image',
+    twitterSite: 'pegipegi',
+    twitterTitle: title,
+    twitterDescription: seoDescription,
+    twitterCreator: '@pegipegi',
+    twitterImage: seoImage,
+    twitterPlayerWidth: seoImageWidth,
+    twitterPlayerHeight: seoImageHeight,
+    twitterPlayer: 'https://www.pegipegi.com/flight',
+  });
+
   useAuth();
 
   const { initiateProfile } = useProfile();
