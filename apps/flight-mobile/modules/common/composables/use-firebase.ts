7c5da3b4036126cc7b054462b2f97ecd733bc234
Andi Ersaldy Raisha Pakki <andi.ersaldy@pegipegi.com>
2023-02-10 14:53:41 +0700

    fix: remove console.log

--- apps/flight-mobile/modules/common/composables/use-firebase.ts
+++ apps/flight-mobile/modules/common/composables/use-firebase.ts
@@ -29,13 +29,6 @@ export default function useFirebase() {
         analytics.value = getAnalytics();
       }
 
-      console.log({
-        customer_id: userId.value,
-        device_id: deviceId.value,
-        product_name: 'Flight',
-        ...params,
-      });
-
       logEvent(analytics.value, type, {
         customer_id: userId.value,
         device_id: deviceId.value,
