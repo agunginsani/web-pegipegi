61a5495de276be48a3e8b2c1a8f9475a884ed541
Agung Insani Alam <agunginsanialam@gmail.com>
2023-06-16 09:41:58 +0700

    chore: migrate to datadog RUM (#188)
    Co-authored-by: Bimorio <67772857+Bimorio@users.noreply.github.com>

--- apps/main-desktop/nuxt.config.ts
+++ apps/main-desktop/nuxt.config.ts
@@ -1,21 +1,9 @@
 import { resolve } from 'path';
 import { version as appVersion } from './package.json';
 
-const script = [];
-if (process.env.ENV_MODE === 'production') {
-  script.push({
-    hid: 'new-relic',
-    id: 'new-relic',
-    src: 'https://new-assets.s3.ap-southeast-1.amazonaws.com/scripts/new-relic-revamp/main-desktop-web.js',
-  });
-}
-
 export default defineNuxtConfig({
   app: {
     buildAssetsDir: '/_assets/',
-    head: {
-      script,
-    },
   },
   modules: [
     '@vueuse/nuxt',
@@ -53,6 +41,7 @@ export default defineNuxtConfig({
       authBaseUrl: '',
       apixBaseUrl: '',
       gtmId: '',
+      envMode: '',
     },
   },
   css: ['~/assets/css/global.css'],
---
+++ apps/main-desktop/plugins/datadog.client.ts
@@ -0,0 +1,25 @@
+import { datadogRum } from '@datadog/browser-rum';
+
+export default defineNuxtPlugin(() => {
+  const {
+    appVersion,
+    public: { envMode },
+  } = useRuntimeConfig();
+
+  datadogRum.init({
+    applicationId: 'ae60728d-465a-47da-94eb-6127be526e7d',
+    clientToken: 'pub744fea693a6f5ad3031f29f16d5b6fc7',
+    site: 'ap1.datadoghq.com',
+    service: 'web-main-desktop',
+    env: envMode,
+    version: appVersion,
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  });
+
+  datadogRum.startSessionReplayRecording();
+});
